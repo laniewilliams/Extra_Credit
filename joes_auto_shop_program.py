27a2f136781c610604071e8aeb84bc7283048aec
Lanie Williams <lanie_williams@baylor.edu>
2022-02-28 15:46:03 -0600

    joe's autoshop

--- joes_auto_shop_program.py
+++ joes_auto_shop_program.py
@@ -7,6 +7,15 @@ def main():
     my_car = car.Car('Toyota','4-Runner','2016')
     my_sq = sq.ServiceQuote(500, 200)
 
+    print("Name: ",my_cust.get_name())
+    print("Address: ",my_cust.get_address())
+    print("Phone: ",my_cust.get_phone())
+    print('-------------------------')
+    print("Manufacturer: ",my_car.get_make())
+    print("Model: ",my_car.get_model())
+    print("Year: $",my_car.get_year())
+    print('-------------------------')
+
     print("Labor Charge:", my_sq.get_labor_charges())
     print("Parts Charge:", my_sq.get_parts_charges())
     print("Sales Tax:", my_sq.get_sales_tax())
