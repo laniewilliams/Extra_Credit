efe0a3eb22f4a3641bc707eab87246bf1db4221d
Lanie Williams <lanie_williams@baylor.edu>
2022-02-28 13:07:58 -0600

    joes autoshop

---
+++ joes_auto_shop_program.py
@@ -0,0 +1,13 @@
+import customer_class as cust
+import car_class as car
+import service_quote_class as sq
+
+def main():
+    my_cust = cust.Customer('Lanie Williams','1411 Paseo de Vaca San Angelo, TX 76901','325-656-4124')
+    my_car = car.Car('Toyota','4-Runner','2016')
+    my_sq = sq.ServiceQuote(500, 200)
+
+   
+
+
+main()
